21d5834220e8abcd28feb5c00326d02f6200d8e1
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-20 02:06:32 +0530

    feat : error page added

---
+++ src/pages/Error.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Error/Error.css";
+
+export const Error = () => {
+  return (
+    <>
+      <h1 className="d-center">Error 404! Page not found</h1>
+    </>
+  );
+};
